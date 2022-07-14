006487a7407e9f29ca0e5e7d5602908cce0b5013
승환 <ghks123285@gmail.com>
2022-07-14 16:39:03 +0900

    feat : process

--- js/main.js
+++ js/main.js
@@ -1,65 +1,77 @@
 
 
+const getBrowserName =()=>{
 
-const getBrowserVesrion = () => {
+  let browser ="unkown";
+
   const userAgent = navigator.userAgent;
-  let browser = "unkown";
+  // 브라우저 이름
 
-  // Chrome browser => test
-  browser = (/chrome|crios/i).test(userAgent) && !(/opr|opera|chromium|edg|ucbrowser|googlebot/i).test(userAgent) ? 'Chrome' : browser;
+  // Chrome browser
+  browser = (/chrome/i)
+    .test(userAgent)
+    ? 'Chrome' : browser;
 
-  // safari browser => test
-  browser = (/safari/i).test(userAgent) && !(/chromium|edg|ucbrowser|chrome|crios|opr|opera|fxios|firefox/i).test(userAgent) ? 'Safari' : browser;
+  // Safari browser
+  browser = (/safari/i)
+    .test(userAgent) &&
+  !(/chromium|edg|ucbrowser|chrome|crios|opr|opera|fxios|firefox/i)
+    .test(userAgent)
+    ? 'Safari' : browser;
 
-  // Edge browser=> test
-  browser=(/edg/i).test(userAgent) ? 'Edge' : browser;
+  // Edge browser
+  browser=(/edg/i)
+    .test(userAgent)
+    ? 'Edge' : browser;
 
-  // Firebase browser => test
-  browser = (/firefox|fxios/i).test(userAgent) && !(/seamonkey/i).test(userAgent) ? 'Firefox' : browser;
+  // Firebase browser
+  browser = (/firefox|fxios/i)
+    .test(userAgent)
+  && !(/seamonkey/i)
+    .test(userAgent)
+    ? 'Firefox' : browser;
 
-  // IE browser => test
-  browser = (/; msie|trident/i).test(userAgent) && !(/ucbrowser/i).test(userAgent) ? 'IE' : browser;
+  return browser;
+}
 
 
-  // browser version check
-  switch (browser) {
+const getBrowserVersion = () => {
 
+  const userAgent = navigator.userAgent;
+
+  let browserName = getBrowserName();
+
+  switch (browserName) {
     case 'Chrome':
-        return `${browser}/${browserVersion(userAgent,/(chrome)\/([\d\.]+)/i)}`;
-    case 'Safari':
-        return  `${browser}/${browserVersion(userAgent,/(safari)\/([\d\.]+)/i)}`;
-    case 'Edge':
-        return `${browser}/${browserVersion(userAgent,/(edge|edga|edgios|edg)\/([\d\.]+)/i)}`;
-    case 'Firefox':
-        return `${browser}/${browserVersion(userAgent,/(firefox|fxios)\/([\d\.]+)/i)}`;
+      return `${browserName}/${browserVersion(userAgent, /(chrome)\/([\d\.]+)/i)}`;
 
-    case 'IE': const version = browserVersion(userAgent,/(trident)\/([\d\.]+)/i);
-      // IE version is mapped using trident version
-      // IE/8.0 = Trident/4.0, IE/9.0 = Trident/5.0
-      return version ? `${browser}/${parseFloat(version) + 4.0}` : `${browser}/7.0`;
+    case 'Safari':
+      return `${browserName}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}`;
+
+    case 'Edge':
+      return `${browserName}/${browserVersion(userAgent, /(edg)\/([\d\.]+)/i)}`;
+
+    case 'Firefox':
+      return `${browserName}/${browserVersion(userAgent, /(firefox)\/([\d\.]+)/i)}`;
+
 
     default :
-        return 'unkown/0.0.0.0';
+      return 'unkown/0.0.0.0';
 
   }
+
+
 }
 
 
 const browserVersion = (userAgent,regex) => {
-  return userAgent.match(regex) ? userAgent.match(regex)[2] : null;
+
+  return userAgent.match(regex)[2];
+
+  //return userAgent.match(regex) ? userAgent.match(regex)[2];
 }
 
-
-const getBrowseTest=()=>{
-
-  let userAgent2 = navigator.userAgent;
-
-
-  return userAgent2;
-}
-
-
-alert(getBrowserVesrion())
+alert(getBrowserVersion());
 
 //
 // }
