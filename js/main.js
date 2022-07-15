6e3e3238426dc48b0f32cec4ffeea09594d897db
승환 <ghks123285@gmail.com>
2022-07-15 14:28:20 +0900

    feat : process4

--- js/main.js
+++ js/main.js
@@ -1,41 +1,29 @@
-
-
 const getBrowserName =()=>{
-
-  let browser ="unkown";
+  let browser = "unkown";
 
   const userAgent = navigator.userAgent;
   // 브라우저 이름
 
   // Chrome browser
-  browser = (/chrome|crios/i)
-    .test(userAgent)
+  browser = (/chrome|chrios/i).test(userAgent)
     ? 'Chrome' : browser;
 
   // Safari browser
-  browser = (/safari/i)
-    .test(userAgent) &&
-  !(/chromium|edg|ucbrowser|chrome|crios|opr|opera|fxios|firefox/i)
-    .test(userAgent)
+  browser = (/safari/i).test(userAgent) &&
+  !(/chromium|edg|ucbrowser|chrome|crios|opr|opera|fxios|firefox/i).test(userAgent)
     ? 'Safari' : browser;
 
   // Edge browser
-  browser=(/edg/i)
-    .test(userAgent)
+  browser = (/edg/i).test(userAgent)
     ? 'Edge' : browser;
 
-
-
   // Firebase browser
-  browser = (/firefox|fxios/i)
-    .test(userAgent)
-  && !(/seamonkey/i)
+  browser = (/firefox|fxios/i).test(userAgent) && !(/seamonkey/i)
     .test(userAgent)
     ? 'Firefox' : browser;
-
   return browser;
-}
 
+}
 
 
 
@@ -45,36 +33,79 @@ const getBrowserVersion = () => {
 
   let browserName = getBrowserName();
 
-  switch (browserName) {
-    case 'Chrome':
-      return `${browserName}/${browserVersion(userAgent, /(chrome|crios)\/([\d\.]+)/i)}`;
-
-    case 'Safari':
-      return `${browserName}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}`;
-
-    case 'Edge':
-      return `${browserName}/${browserVersion(userAgent, /(edge|edga|edgios|edg)\/([\d\.]+)/i)}`;
-
-    case 'Firefox':
-      return `${browserName}/${browserVersion(userAgent, /(firefox)\/([\d\.]+)/i)}`;
 
 
-    default :
-      return 'unkown/0.0.0.0';
+  let pcDevice = "win|16|win32|win65|mac|macintel";
+
+  if(navigator.platform){
+
+    if(pcDevice.indexOf(navigator.platform.toLowerCase())<0){
+      switch (browserName) {
+        case 'Chrome':
+          // crios 모바일 크롬 브라우저
+          // chrome Pc 크롬 브라우저
+          return `${browserName}/${browserVersion(userAgent, /(chrome|crios)\/([\d\.]+)/i)}+ Mobile`;
+
+        case 'Safari':
+          return `${browserName}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}+ Mobile`;
+
+        case 'Edge':
+          return `${browserName}/${browserVersion(userAgent, /(edge|edga|edgios|edg)\/([\d\.]+)/i)}+ Mobile`;
+        // firefox pc 크롬 브라우저
+        // fxios 모바일 크롬 브라우저
+
+        case 'Firefox':
+          return `${browserName}/${browserVersion(userAgent, /(firefox|fxios)\/([\d\.]+)/i)} + Mobile`;
+
+        default :
+          return 'unkown/0.0.0.0';
+
+      }
+
+    }else{
+
+      switch (browserName) {
+        case 'Chrome':
+          // crios 모바일 크롬 브라우저
+          // chrome Pc 크롬 브라우저
+          return `${browserName}/${browserVersion(userAgent, /(chrome|crios)\/([\d\.]+)/i)}+ PC`;
+
+        case 'Safari':
+          return `${browserName}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}+ PC`;
+
+        case 'Edge':
+          return `${browserName}/${browserVersion(userAgent, /(edge|edga|edgios|edg)\/([\d\.]+)/i)}+ PC`;
+        // firefox pc 크롬 브라우저
+        // fxios 모바일 크롬 브라우저
+
+        case 'Firefox':
+          return `${browserName}/${browserVersion(userAgent, /(firefox|fxios)\/([\d\.]+)/i)} + PC`;
+
+        default :
+          return 'unkown/0.0.0.0';
+
+      }
+
+      console.log("PC");
+    }
 
   }
 
 
+
+
 }
+
+
+
 
 
 
 const browserVersion = (userAgent,regex) => {
-  // return userAgent.match(regex)[2];
-  return userAgent.match(regex) ? userAgent.match(regex)[2]: null;
+  return userAgent.match(regex)[2];
 }
+
+
 
 alert(getBrowserVersion());
 
-//
-// }
